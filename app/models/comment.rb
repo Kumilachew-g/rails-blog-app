d5da9a05ca84b0f4f75228e308709dd86c822357
kumilachew-g <kumilachew1212@gmail.com>
2022-11-24 13:20:35 +0300

    Switch to comments_counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
 
-  after_save :update_post_comment_counter
+  after_save :comments_counter
 
   def comments_counter
     post.increment!(:comments_counter)
