db7cee5a1ba8fe13de29981cf8ab3d2d5d1c94e6
kumilachew-g <kumilachew1212@gmail.com>
2022-11-24 13:19:58 +0300

    Switch to like_counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
 
-  after_save :update_post_like_counter
+  after_save :like_counter
 
   def likes_counter
     post.increment!(:likes_counter)
