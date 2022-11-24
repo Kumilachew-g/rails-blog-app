e4f06b2716cfdf3948de88383f18d4e85f0c5ab4
kumilachew-g <kumilachew1212@gmail.com>
2022-11-24 10:44:58 +0300

    Add association in class model comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
 
+  after_save :update_post_comment_counter
+
   def comments_counter
     post.increment!(:comments_counter)
   end
