a1773099ee81c7bd915c839b9cbdfe26f52846f1
kumilachew-g <kumilachew1212@gmail.com>
2022-11-24 10:44:02 +0300

    Add after_save association in class model like

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User'
 
+  after_save :update_post_like_counter
+
   def likes_counter
     post.increment!(:likes_counter)
   end
