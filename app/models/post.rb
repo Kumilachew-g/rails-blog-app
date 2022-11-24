3adcee6841ed463f6ff6d41a5755b70cadec87da
kumilachew-g <kumilachew1212@gmail.com>
2022-11-24 10:39:03 +0300

    Add after_save  association in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
 
   belongs_to :author, class_name: 'User'
 
+  after_save :update_post_user_counter
+
   def post_counter
     author.increment!(:posts_counter)
   end
