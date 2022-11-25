9c2145895fae55fbc04dd7159d9b23fe2e30de99
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-25 11:30:05 +0300

    Add tests for name valid and posts_counter valid

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Kumie', photo: 'https://unsplash.com/photos/r-QpUNyC5Bc', bio: 'Student from Microverse.') }
+  subject { User.new(name: 'Kumie', photo: 'https://unsplash.com/photos/r-QpUNyC5Bc', bio: 'Student from Microverse.', posts_counter: 0) }
 
   before { subject.save }
 
@@ -10,8 +10,17 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
+  it 'name should be valid' do
+   expect(subject).to be_valid
+ end
+
   it 'posts_counter should be greater_than_or_equal zero' do 
    subject.posts_counter = -1
    expect(subject).to_not be_valid
+ end
+
+ it 'posts_counter be valid' do 
+   subject.posts_counter = 1
+   expect(subject).to be_valid
  end
 end
