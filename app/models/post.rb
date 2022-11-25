a329a2f043ebc83d9ec43031092241fae013b9c4
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-25 10:37:32 +0300

    Add comments_counter validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,7 @@ class Post < ApplicationRecord
 
   validates :title, presence: true,  length: { maximum: 250 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   private
 
