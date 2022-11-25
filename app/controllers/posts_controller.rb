c14d7bee120f59d80e412e5fa5fc93512d168d98
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-25 20:19:24 +0300

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,35 +1,35 @@
 class PostsController < ApplicationController
-    def show
-        @user = User.find_by_id(params[:user_id])
-        @post = @user.posts.includes(:likes, :comments).find(params[:id])
-      end
-    
-      def index
-        @user = User.includes(:posts).find(params[:user_id])
-        @comments = @user.posts.includes(:comments)
-      end
-    
-      def new
-        @current = current_user
-      end
-    
-      def create
-        created_post = current_user.posts.build(post_params)
-    
-        respond_to do |format|
-          format.html do
-            if created_post.save
-              redirect_to user_post_path(created_post.author_id, created_post.id), notice: 'Post created !'
-            else
-              render :new, alert: 'Post not created, please try again!'
-            end
-          end
+  def show
+    @user = User.find_by_id(params[:user_id])
+    @post = @user.posts.includes(:likes, :comments).find(params[:id])
+  end
+
+  def index
+    @user = User.includes(:posts).find(params[:user_id])
+    @comments = @user.posts.includes(:comments)
+  end
+
+  def new
+    @current = current_user
+  end
+
+  def create
+    created_post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      format.html do
+        if created_post.save
+          redirect_to user_post_path(created_post.author_id, created_post.id), notice: 'Post created !'
+        else
+          render :new, alert: 'Post not created, please try again!'
         end
       end
-    
-      private
-    
-      def post_params
-        params.require(:post).permit(:title, :text)
-      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,10 @@ class Post < ApplicationRecord
 
   after_save :post_counter
 
-  validates :title, presence: true,  length: { maximum: 250 }
+  validates :title, presence: true, length: { maximum: 250 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  
   def post_counter
     user.increment!(:posts_counter)
   end
