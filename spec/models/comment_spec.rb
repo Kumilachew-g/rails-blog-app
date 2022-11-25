262c4e546e68da740b81fba38130564425a7b577
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-25 20:17:57 +0300

    Add tests for comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,23 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-    user1 = User.new
-      post1 = Post.new(title: 'post_test', text:"this is post test", author:user1, likes_counter:8)
-    subject do
-      Comment.new
-    end
-    before {subject.save}
-    it "title presence" do
-      subject.text = nil
-      expect(subject).to_not be_valid
-    end
+  subject do
+    Comment.new(
+      post: Post.new(
+        user: User.new(
+          name: 'Haddis',
+          photo: 'https://unsplash.com/photos/-rwqO6Jvo_M',
+          bio: 'Teacher from AASTU.',
+          posts_counter: 0
+        ),
+        title: 'Java',
+        text: 'This is my first post',
+        likes_counter: 0,
+        comments_counter: 0
+      ),
+      user: User.new(name: 'Haddis', photo: 'https://unsplash.com/photos/-rwqO6Jvo_M', bio: 'Teacher from AASTU.',
+                     posts_counter: 0),
+      text: 'This is the comment'
+    )
+  end
 
-    # describe "comments model method" do
-    #   it "should update comments count" do
-    #     post1.comments_counter = 2
-    #     subject.post = post1
-    #     expect(subject.comments_counter.comments_counter).to eq 3
-    #   end
-    # end
+  before { subject.save }
+
+  it 'update_post_comments_counter should return a right result when calling it with after_save' do
+    expect(subject.post.comments_counter).to eq(1)
   end
 end
