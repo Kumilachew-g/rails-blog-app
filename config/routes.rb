7dc332855eaa670dea59fd540ab2f2b2884fb93e
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-29 10:09:47 +0300

    Add route for posts controller show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] 
-  resources :posts, on: :show, only: [:index]
+  resources :posts, on: :show, only: [:index, :show]
 end
