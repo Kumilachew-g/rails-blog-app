afc72d4ae83fe7acaebe69ebc7070209a27353c8
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-30 11:05:08 +0300

    Undo response status test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,9 +3,15 @@ require 'rails_helper'
 RSpec.describe 'Users', type: :request do
     describe "GET /index" do
         before(:example) { get users_path } 
+
+        it 'Is response status correct' do
+            expect(response).to have_http_status(:ok)
+          end
     
         it "Check is correct template rendered" do
             expect(response).to render_template('index')
           end
       end
+
+
 end
