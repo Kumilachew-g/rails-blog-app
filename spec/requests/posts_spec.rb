65d698b74ddee59a2cd532f27f34736b61753c97
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-30 12:35:17 +0300

    Fix linter/rubocop errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -17,20 +17,18 @@ RSpec.describe 'Posts', type: :request do
     end
   end
 
+  describe 'GET /show' do
+    before(:example) { get '/posts/:post_id' }
 
-  
-  describe "GET /show" do
-    before(:example) { get '/posts/:post_id' } 
-
-    it "Is response status correct" do
+    it 'Is response status correct' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "Is correct template rendered" do
+    it 'Is correct template rendered' do
       expect(response).to render_template(:show)
     end
 
-    it "Is the body includes correct text" do
+    it 'Is the body includes correct text' do
       expect(response.body).to include('Show')
     end
   end
