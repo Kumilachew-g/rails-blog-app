4939f589c2a808fde36bf990911db8bc11042793
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-30 12:34:21 +0300

    Add post_spec tests for posts show action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,11 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-    describe 'GET /index' do
-        before(:example) { get '/users/posts' }
-    
-        it 'Is response status correct' do
-          expect(response).to have_http_status(:ok)
-        end
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    before(:example) { get '/posts' }
+
+    it 'Is response status correct' do
+      expect(response).to have_http_status(:ok)
     end
+
+    it 'Is correct template rendered' do
+      expect(response).to render_template(:index)
+    end
+
+    it 'Is the body includes correct text' do
+      expect(response.body).to include('INDEX')
+    end
+  end
+
+
+  
+  describe "GET /show" do
+    before(:example) { get '/posts/:post_id' } 
+
+    it "Is response status correct" do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "Is correct template rendered" do
+      expect(response).to render_template(:show)
+    end
+
+    it "Is the body includes correct text" do
+      expect(response.body).to include('Show')
+    end
+  end
 end
