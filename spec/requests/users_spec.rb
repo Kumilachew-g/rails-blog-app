5292ea49f4526922234787dfcad9f875d11d601b
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-30 11:01:42 +0300

    Add real template rendered test for index users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe 'Users', type: :request do
     describe "GET /index" do
         before(:example) { get users_path } 
     
-        it "Is response status correct" do
-          expect(response).to have_http_status(:ok)
-        end
+        it "Check is correct template rendered" do
+            expect(response).to render_template('index')
+          end
       end
 end
