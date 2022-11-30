75bcf4eafed37bbe973379c5482297e513e31b0a
Kumilachew-g <kumilachew1212@gmail.com>
2022-11-30 11:25:38 +0300

    Switch from get users_path to get '/users'

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,19 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
     describe "GET /index" do
-        before(:example) { get users_path } 
+        before(:example) { get '/users' }
 
         it 'Is response status correct' do
             expect(response).to have_http_status(:ok)
           end
     
         it "Check is correct template rendered" do
-            expect(response).to render_template('index')
+            expect(response).to render_template(:index)
+          end
+
+          it "Check is the body includes correct text" do
+            expect(response.body).to include('INDEX')
           end
       end
-
-      it "Check is the body includes correct text" do
-        expect(response.body).to include('Index')
-      end
-
 end
