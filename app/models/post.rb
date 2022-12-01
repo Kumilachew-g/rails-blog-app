95c190c0b61902e18cc9dcb3dc5a03d1c7c386f8
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-01 20:49:46 +0300

    Add enhancements for the methods name

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,17 +3,17 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :user
 
-  after_save :post_counter
+  after_save :update_post_user_counter
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def post_counter
+  def update_post_user_counter
     user.increment!(:posts_counter)
   end
 
-  def recent_comments
-    comments.order(created_at: :desc).limit(5)
+  def five_most_recent_comments
+    comments.last(5)
   end
 end
