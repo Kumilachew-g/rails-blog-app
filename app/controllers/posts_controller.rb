d3f00ec1e68d78bf899daa4b1b198fa7f672a4df
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-01 20:52:07 +0300

    fix linter/rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-   end
+  end
 
   def show
     @post = User.find(params[:user_id]).posts.find(params[:id])
