73907899b3793edf27f773fee8cd779c047de080
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-02 12:30:39 +0300

    Add new comment functions to index post view

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,7 @@ class LikesController < ApplicationController
         respond_to do |format|
           format.html do
             if like.save
-              flash[:success] = "Likes saved successfully"
+              flash[:success] = "Like saved successfully"
               redirect_to "/users/#{current_user.id}/posts"
             else
               flash.now[:error] = "Error!: Post could not be saved"
