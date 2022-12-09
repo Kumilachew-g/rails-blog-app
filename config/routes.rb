9aa68b8cf32005807f1b6397a1ebadd765828190
Kumilachew-g <kumilachew1212@gmail.com>
2022-12-09 11:12:22 +0300

    Add API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api do
+    post 'users/sign_in' => 'users#login'
+    get 'posts' => 'posts#index'
+    get 'comments' => 'comments#index'
+    post 'comments/create' => 'comments#create'
+  end
+
   get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
   post 'likes/:post_id', to: 'likes#create'
